670fc90e465d072d44a08c012e0d984894d2ec6d
Yanic Kruck <y.kruck@option47.de>
2022-04-02 21:47:20 +0200

    added Xamarin mobile projects

---
+++ SatStackerMobile/SatStackerMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SatStackerMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SatStackerMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ SatStackerMobile/SatStackerMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SatStackerMobile.Models;
+using SatStackerMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SatStackerMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
